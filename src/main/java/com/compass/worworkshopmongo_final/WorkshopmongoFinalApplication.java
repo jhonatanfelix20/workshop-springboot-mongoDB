d3c0ffda80d35535d77ef6f72c832be195399862
Jhonatan Felix <jhonatanvfelix@gmail.com>
2024-06-14 14:22:06 -0300

    Primeiro commit

---
+++ src/main/java/com/compass/worworkshopmongo_final/WorkshopmongoFinalApplication.java
@@ -0,0 +1,13 @@
+package com.compass.worworkshopmongo_final;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopmongoFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopmongoFinalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/compass/worworkshopmongo_final/WorkshopmongoFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.compass.worworkshopmongo_final;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
