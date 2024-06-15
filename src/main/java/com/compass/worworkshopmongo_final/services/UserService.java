65165639cefb61e5f976efee9def422656d6edce
Jhonatan Felix <jhonatanvfelix@gmail.com>
2024-06-14 23:18:34 -0300

    Obtendo um usuário por id

--- src/main/java/com/compass/worworkshopmongo_final/services/UserService.java
+++ src/main/java/com/compass/worworkshopmongo_final/services/UserService.java
@@ -1,12 +1,14 @@
 package com.compass.worworkshopmongo_final.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.compass.workshopmongo_final.domain.User;
 import com.compass.worworkshopmongo_final.repository.UserRepository;
+import com.compass.worworkshopmongo_final.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -19,4 +21,8 @@ public class UserService {
 		
 	}
 
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
 }
