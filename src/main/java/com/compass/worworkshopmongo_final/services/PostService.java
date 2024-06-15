88ecf0cadd78d59a979cdf04f5eae8cdc66609b3
Jhonatan Felix <jhonatanvfelix@gmail.com>
2024-06-15 01:02:50 -0300

    Obtendo um post por id

---
+++ src/main/java/com/compass/worworkshopmongo_final/services/PostService.java
@@ -0,0 +1,24 @@
+package com.compass.worworkshopmongo_final.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.compass.workshopmongo_final.domain.Post;
+import com.compass.worworkshopmongo_final.repository.PostRepository;
+import com.compass.worworkshopmongo_final.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+}
