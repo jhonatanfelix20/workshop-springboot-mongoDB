f4045a7a765d3f0f9b3db9b7a80e4f56483fbb60
Jhonatan Felix <jhonatanvfelix@gmail.com>
2024-06-14 22:32:12 -0300

    Usando padrão DTO para retornar usuários

---
+++ src/main/java/com/compass/worworkshopmongo_final/dto/UserDTO.java
@@ -0,0 +1,49 @@
+package com.compass.worworkshopmongo_final.dto;
+
+import java.io.Serializable;
+
+import com.compass.workshopmongo_final.domain.User;
+
+public class UserDTO implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	private String id;
+	private String nome;
+	private String email;
+	
+	public UserDTO () {
+		
+	}
+	
+	public UserDTO(User obj) {
+		id = obj.getId();
+		nome = obj.getnome();
+		email = obj.getEmail();
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+}
